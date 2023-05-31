afedb772fbd28b070e4a7d1a9bb2b4b9f9453e52
Mikael <kaelbaracho@gmail.com>
2023-05-30 22:21:32 -0300

    Carrossel, importando SwiperJS

---
+++ scripts/carrossel-swiperJS.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    spaceBetween: 5,
+    slidesPerView: 2.4,
+
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+  });
