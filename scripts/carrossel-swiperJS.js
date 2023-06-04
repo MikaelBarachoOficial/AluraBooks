c65cca81854206b51ac8bc442403d5f30900611d
Mikael <kaelbaracho@gmail.com>
2023-06-04 19:48:56 -0300

    Responsivo Tablet e Footer Concluído

--- scripts/carrossel-swiperJS.js
+++ scripts/carrossel-swiperJS.js
@@ -1,7 +1,34 @@
-const swiper = new Swiper('.swiper', {
+if (window.innerWidth >= 750) {
+
+  const swiper = new Swiper('.swiper', {
+
+      speed: 400,
+      spaceBetween: 80,
+      slidesPerView: 3,
+    
+      pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+        },
+
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+
+    });
+
+} else {
+
+  const swiper = new Swiper('.swiper', {
+    
     spaceBetween: 5,
     slidesPerView: 2.4,
-
+  
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
@@ -12,3 +39,5 @@ const swiper = new Swiper('.swiper', {
     loop: true,
 
   });
+
+}
